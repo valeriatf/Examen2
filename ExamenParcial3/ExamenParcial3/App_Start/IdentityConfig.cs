9ad08905e21654cb0cd1a81d8e436d2e17e779fc
Valeria Tapia Fusco <valeria.tapia.fusco@gmail.com>
2019-04-06 10:31:03 -0400

    Al fin funciono !!

--- ExamenParcial3/ExamenParcial3/App_Start/IdentityConfig.cs
+++ ExamenParcial3/ExamenParcial3/App_Start/IdentityConfig.cs
@@ -54,7 +54,7 @@ namespace ExamenParcial3
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
+                RequireNonLetterOrDigit = false,
                 RequireDigit = true,
                 RequireLowercase = true,
                 RequireUppercase = true,
---
+++ ExamenParcial3/ExamenParcial3/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ExamenParcial3.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<ExamenParcial3.Models.Friend> Friends { get; set; }
+    }
+}
